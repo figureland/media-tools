e29e1c486376992c1010e43c314f749850f52f69
Toby Milner-Gulland <toby@figure.land>
2024-10-20 21:19:39 +0100

    fix: type mismatch between Buffer and UintArray

--- src/hash.ts
+++ src/hash.ts
@@ -2,9 +2,8 @@ import { readFile } from 'fs/promises'
 import { createHash } from 'crypto'
 
 export const getFileHash = async (inputFile: string) => {
-  const inputHash = createHash('md5')
-    .update(await readFile(inputFile))
-    .digest('hex')
+  const fileContent = await readFile(inputFile)
+  const inputHash = createHash('md5').update(new Uint8Array(fileContent)).digest('hex')
   const shortHash = inputHash.slice(0, 8)
 
   return {
